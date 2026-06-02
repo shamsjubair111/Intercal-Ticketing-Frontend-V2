e99c5be1fdc03ef98d66906d0cab23eacb91eb96
Shams Jubair <jubair.shams@brilliant.com.bd>
2026-06-02 16:32:15 +0600

    base_url changed

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
-export const API_BASE_URL = "http://36.255.70.9:8002";
-// export const API_BASE_URL = "http://127.0.0.1:5000";
+export const API_BASE_URL =
+  typeof window !== "undefined" && window.location.hostname === "internal-v2.brilliant.com.bd"
+    ? "https://internal-v2.brilliant.com.bd"
+    : "http://36.255.70.9:8002";
